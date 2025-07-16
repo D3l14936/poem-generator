0e5739c814d719e2787c04b408bd70180e13ed11
D3l14936 <yuu.miyamichi@gmail.com>
2025-07-16 20:10:06 +0200

    poem>haiku, added API, prompt and loading, adjusted UI (colors, width, font)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function generatePoem(event) {
-    event.preventDefault();
+function displayPoem(response) {
+  let poemContainer = document.querySelector("#poem");
 
-    new Typewriter("#poem", {
-      strings: "poem goes here",
-      autoStart: true,
-      delay: 40,
-      cursor: "",
-    });
-  }
+  // typewriter
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+    loop: false,
+    pauseFor: 2500,
+  });
+}
 
-  let poemElement = document.querySelector("#poem-form");
-  poemElement.addEventListener("submit", generatePoem);
-});
+// api data + prompt
+function generatePoem(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#user-input");
+  let apiKey = "f56ddo443f56b76637t0be5d36d0503a";
+  let prompt = `Generate a haiku about ${userInput.value}, separate lines with <br>, please.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&key=${apiKey}`;
+
+  // show loading message
+  let poemContainer = document.querySelector("#poem");
+  poemContainer.innerHTML = "Loading...";
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+let poemElement = document.querySelector("#poem-form");
+poemElement.addEventListener("submit", generatePoem);
